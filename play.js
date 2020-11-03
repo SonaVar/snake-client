efd96dcdf68d47a28a0c616ad9851d5a2aeb4623
Sona Varghese <sonasusan.v@gmail.com>
2020-11-03 22:09:42 +0000

    Commit stdin changes to play.js

--- play.js
+++ play.js
@@ -1,7 +1,32 @@
-const net = require('net');
+//const net = require('net');
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
 
 const connect = require('./client');
-
 console.log('Connecting ...');
-
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+setupInput();
+
